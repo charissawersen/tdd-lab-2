ed10acc40c386d9e9f13b12b166afd7ba7483e50
Charissa Wersen <charissa.wersen@gmail.com>
2020-12-03 19:38:41 -0500

    adding changes

--- test/translate.test.js
+++ test/translate.test.js
@@ -1,15 +1,47 @@
-describe('translate', function(){
+describe('translate', function ()
+{
     // 1. Multiple tests for words starting with different vowels.
-    it('word starting with a', function(){
+    it('word starting with a', function ()
+    {
         expect(translate('acne')).toEqual('acneway')
+
+    })
+    it('word starting with e', function ()
+    {
+        expect(translate('elephant')).toEqual('elephantway')
+
+    })
+    it('word starting with i', function ()
+    {
+        expect(translate('igloo')).toEqual('iglooway')
+    })
+    it('word starting with o', function ()
+    {
+        expect(translate('outside')).toEqual('outsideway')
+    })
+    it('word starting with u', function ()
+    {
+        expect(translate('ugly')).toEqual('uglyway')
     })
 });
 
 // 2. At least one test for words starting with one consonant.
-
+describe('translate', function ()
+{
+    it('word starting with one constanant', function ()
+    {
+        expect(translate('card')).toEqual('ardcay')
+    })
+});
 
 // 3. At least one test for words starting with two consonants.
-
+describe('translate', function ()
+{
+    it('word starting with two constanant', function ()
+    {
+        expect(translate('stealth')).toEqual('ealthstay')
+    })
+});
 
 // 4. At least one test for words starting with three consonants. 
 
--- translate.js
+++ translate.js
@@ -1,9 +1,22 @@
-let vowels = ['a', 'e', 'i', 'o', 'u']; 
-let newString = ""; 
+let vowels = ['a', 'e', 'i', 'o', 'u'];
+let newWord = "";
 
-function translate (str) {
-if(vowels.indexOf(str[0]) > -1) {
-    newString = str + 'way'; 
-    return newString; 
-}
-}
+function translate(str)
+{
+    if (vowels.indexOf(str[0]) > -1)
+    {
+        newWord = str + 'way';
+        return newWord;
+    
+} else {
+    let matchVowels = str.match(/['a', 'e', 'i', 'o', 'u']/g) || 0; 
+    //console.log(matchVowels);
+    let firstVowel = str.indexOf(matchVowels[0]);
+    //console.log(firstVowel);
+    newWord = str.substring(firstVowel) + str.substring(0, firstVowel) + 'ay';
+    //console.log(newWord);
+    
+    // str.substring(firstVowel) + str.substring(0, firstVowel) + 'ay';
+    return newWord;
+}}
+//console.log(translate('scared'));
